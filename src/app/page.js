e024181e256dcd5d1ac15ee38d5fd256fe1b7f7f
vic3 <kkvic4@gmail.com>
2024-05-22 14:45:16 +0100

    hello world

--- src/app/page.js
+++ src/app/page.js
@@ -9,8 +9,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          Hello World
+          
         </p>
         <div>
           <a
@@ -32,17 +32,10 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+        
+         
+         <h1>Hello World</h1> 
+        
       </div>
 
       <div className={styles.grid}>
